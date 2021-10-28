2ba7ccd9b36dcf10ddca62879ce228d259ecdcb7
Zhang <504947985@qq.com>
2021-10-28 10:44:55 +0800

    修复编码错误，运行命令行的方式改成 subprocess.run

--- getCacheWiFiPassword.py
+++ getCacheWiFiPassword.py
@@ -2,19 +2,26 @@
 # 简体中文，中间用到正则表达式，我不知道在别的系统语言下
 # 关键字是什么。
 
-
-import os
 import re
+import subprocess
 
 # netsh wlan show profile
 # netsh wlan show profile “WiFi名称” key=clear
 
-wifi_info = os.popen('netsh wlan show profile').read()
+
+def run_command(command):
+    process = subprocess.run(command,
+                             stdout=subprocess.PIPE,
+                             shell=True)
+    out = process.stdout.decode('gbk', 'ignore')
+    return out
+
+wifi_info = run_command('netsh wlan show profile')
 wifi_list = re.findall(r'.+:\s(\S+)', wifi_info)
 
 
 def get_wifi_password(wifi):
-    data = os.popen(f'netsh wlan show profile {wifi} key=clear').read()
+    data = run_command(f'netsh wlan show profile {wifi} key=clear')
     password_list = re.findall(r'关键内容.+:\s(.+)', data)
     if password_list:
         password = password_list[0]
@@ -22,8 +29,9 @@ def get_wifi_password(wifi):
         password = ''
     return password
 
-print('{:<20s}{:<20s}'.format('WIFI', 'PASSWORD'))
+print('{:<20s}{:<20s}\n'.format('WIFI', 'PASSWORD'))
 for wifi in wifi_list:
     print('{:<20s}{:<20s}'.format(wifi, get_wifi_password(wifi)).strip())
 
-os.system('pause')
+print()
+subprocess.run('pause', shell=True)
